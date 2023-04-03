9b92c82c343584e53e40ecc7511b11a27fe4da29
User <silvia.ant2407@gmail.com>
2023-04-03 19:59:02 +0700

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+    const windowPosition = window.scrollY > 0;
+    navbar.classList.toggle
+    ("scrolling-active", windowPosition);
+})
